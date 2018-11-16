323e596c154992e94299c36accc8cf27db0821d1
IsFive <754714343@qq.com>
2018-11-16 11:26:53 +0800

    Update ESUtil.java

--- springboot/src/main/java/com/mn/springboot/utils/es/ESUtil.java
+++ springboot/src/main/java/com/mn/springboot/utils/es/ESUtil.java
@@ -1,15 +1,11 @@
 package com.mn.springboot.utils.es;
 
-import com.mn.springboot.pojo.Teacher;
 import com.mn.springboot.utils.GenericsUtils;
 import org.frameworkset.elasticsearch.client.ClientInterface;
 import org.frameworkset.elasticsearch.entity.ESDatas;
 import org.springframework.stereotype.Service;
 
-import javax.jnlp.BasicService;
-import java.lang.reflect.ParameterizedType;
 import java.util.List;
-import java.util.Map;
 
 /**
  * es工具类
@@ -22,7 +18,7 @@ public class ESUtil {
 	public void addOrUpdateDocument(String index,Object obj){
 		//获取文档的客户端对象，单实例多线程安全
 		ClientInterface clientUtil = ESClient.restClient();
-		//向固定index添加或者修改文档,如果demoId已经存在做修改操作，否则做添加文档操作，返回处理结果
+		//向固定index添加或者修改文档,如果id已经存在做修改操作，否则做添加文档操作，返回处理结果
 		//索引表 索引类型
 		clientUtil.addDocument(index,index,obj);
 	}
@@ -33,7 +29,7 @@ public class ESUtil {
 	public <T> void addOrUpdateDocuments(String index,List<T> list){
 		//获取文档的客户端对象，单实例多线程安全
 		ClientInterface clientUtil = ESClient.restClient();
-		//向固定index添加或者修改文档,如果demoId已经存在做修改操作，否则做添加文档操作，返回处理结果
+		//向固定index添加或者修改文档,id，否则做添加文档操作，返回处理结果
 		//索引表 索引类型
 		clientUtil.addDocuments(index,index,list);
 	}
@@ -81,22 +77,22 @@ public class ESUtil {
 		clientUtil.executeHttp("_bulk",builder.toString(),"post");
 	}
 
-
 	/**
 	 * 通过mapper文件执行查询语句
 	 * @param index 索引
 	 * @param params 传递的参数
 	 * @param dsl 对应mapper文件的name
-	 * @param clientUtil 客户端
 	 */
-	public <T> List<T> getDocumentList(String index,T params,String dsl,ClientInterface clientUtil){
+	public <T> List<T> exec(String index,T params,String dsl){
+		//获取索引对应的客户端
+		ClientInterface clientUtil = ESClient.restClient(index);
 		//设定查询条件,通过map传递变量参数值,key对于dsl中的变量名称
 		//执行查询，index为索引表，_search为检索操作action
 		//使用反射获取Class<T>
-		Class<T> clazz = GenericsUtils.getSuperClassGenricType(params.getClass(), 0);
+		Class<T> clazz = GenericsUtils.getSuperClassGenricType(params.getClass());
 		ESDatas<T> esDatas =  //ESDatas包含当前检索的记录集合，最多1000条记录，由dsl中的size属性指定
 				clientUtil.searchList(index+"/_search",//demo为索引表，_search为检索操作action
-						dsl,//esmapper/demo.xml中定义的dsl语句
+						dsl,//esmapper/xxx.xml中定义的dsl语句的name
 						params,//变量参数
 						clazz);//返回的文档封装对象类型
 		//获取结果对象列表，最多返回1000条记录
